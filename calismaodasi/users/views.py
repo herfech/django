877fa69b5f91a612c3b2075640f3851545e9a711
HERIBERTO FERNANDEZ CHALE <heriberto.fech@gmail.com>
2025-12-05 15:13:21 +0300

    Add files via upload

--- calismaodasi/users/views.py
+++ calismaodasi/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            kullanici = form.save()
+            login(request, kullanici)
+            return redirect("home")
+    else: 
+        form = UserCreationForm()
+    return render(request, "calismaodasi/register.html", {"form": form})
